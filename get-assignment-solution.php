060d70975b8b8bad647657daef03c6055daec048
marcabounader92 <marcabounader92@outlook.com>
2023-07-22 23:02:59 +0300

    PHP: added assignment solution API

---
+++ get-assignment-solution.php
@@ -0,0 +1,37 @@
+<?php
+
+include('Config\db_connect.php');
+
+header("Content-type: application/json; charset=utf-8");
+header('Access-Control-Allow-Origin: http://localhost:5500');
+header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Headers: Content-Type");
+
+
+// $_GET = json_decode(file_GET$_GET_contents('php://input'), true);
+
+$student_id=$_GET['student_id'];
+$assignment_id=$_GET['assignment_id'];
+$solution="";
+$sql = $conn->prepare("select solution from assignments_solution where student_id=? and assignment_id=?;");
+$sql->bind_param("ss",$student_id,$assignment_id);
+$sql->execute();
+$result=$sql->get_result();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$arrayofrows = array();
+
+if(!$result){
+  $response=array("status"=>"0","error"=>"No solutions");
+  echo json_encode($data);
+  exit();
+} else{
+
+    while( $row=$result->fetch_array(MYSQLI_ASSOC)){
+        $arrayofrows[] = $row;
+    }
+    echo json_encode($arrayofrows);
+}
+
+  
+
+?>
