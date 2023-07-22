68a94e065b2822ab572ecda4ea17b3d150fd2369
marcabounader92 <marcabounader92@outlook.com>
2023-07-22 23:03:37 +0300

    PHP: removed credentials header

--- get-student-classes.php
+++ get-student-classes.php
@@ -6,14 +6,13 @@ header("Content-type: application/json; charset=utf-8");
 header('Access-Control-Allow-Origin: http://localhost:5500');
 header('Access-Control-Allow-Methods: POST');
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true");
 
 
 // $_GET = json_decode(file_GET$_GET_contents('php://input'), true);
 
 $student_id=$_GET['student_id'];
 $class_name=$teacher_id=$teacher_name="";
-$sql = $conn->prepare("select class_id from class_students where student_id=?");
+$sql = $conn->prepare("select class_name from classes where class_id IN (select class_id from class_students where student_id=?);");
 $sql->bind_param("s",$student_id);
 $sql->execute();
 $result=$sql->get_result();
@@ -29,29 +28,9 @@ if(!$result){
     while( $row=$result->fetch_array(MYSQLI_ASSOC)){
         $arrayofrows[] = $row;
     }
-
-}
-$arrayofClasses=array();
-foreach($arrayofrows as $row){
-    $sql = $conn->prepare("select class_name from classes where class_id=?");
-    $sql->bind_param("s",$row['class_id']);
-    $sql->execute();
-    $sql->store_result();
-    if($sql->num_rows()==0){
-        $response=array("status"=>"0","error"=>"No class names");
-        echo json_encode($data);
-        exit();
-    } else{
-        $sql->bind_result($class_name);
-        $sql->fetch();
-        $row=array('class_id'=>$row['class_id'],'class_name'=>$class_name);
-        $arrayofClasses[] = $row;
-    }
+    echo json_encode($arrayofrows);
 }
 
-echo json_encode($arrayofClasses);
-//   $response=array('class_id'=>$class_id,'class_name'=>$class_name);
-//   echo json_encode($response);
   
 
 ?>
