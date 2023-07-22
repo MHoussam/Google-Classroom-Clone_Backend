1dc0e534888c0492417a250ae7c4f9fb7e5372c1
marcabounader92 <marcabounader92@outlook.com>
2023-07-22 17:42:30 +0300

    PHP: added get student classes

--- get-student-classes.php
+++ get-student-classes.php
@@ -13,23 +13,43 @@ header("Access-Control-Allow-Credentials: true");
 
 $student_id=$_GET['student_id'];
 $class_name=$teacher_id=$teacher_name="";
-$arrayofrows=array();
 $sql = $conn->prepare("select class_id from class_students where student_id=?");
 $sql->bind_param("s",$student_id);
 $sql->execute();
 $result=$sql->get_result();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$arrayofrows = array();
+
 if(!$result){
   $response=array("status"=>"0","error"=>"No classes");
   echo json_encode($data);
   exit();
 } else{
+
     while( $row=$result->fetch_array(MYSQLI_ASSOC)){
-        $arrayofrows=$row;
+        $arrayofrows[] = $row;
     }
-    echo json_encode($arrayofrows);
 
 }
+$arrayofClasses=array();
+foreach($arrayofrows as $row){
+    $sql = $conn->prepare("select class_name from classes where class_id=?");
+    $sql->bind_param("s",$row['class_id']);
+    $sql->execute();
+    $sql->store_result();
+    if($sql->num_rows()==0){
+        $response=array("status"=>"0","error"=>"No class names");
+        echo json_encode($data);
+        exit();
+    } else{
+        $sql->bind_result($class_name);
+        $sql->fetch();
+        $row=array('class_id'=>$row['class_id'],'class_name'=>$class_name);
+        $arrayofClasses[] = $row;
+    }
+}
+
+echo json_encode($arrayofClasses);
 //   $response=array('class_id'=>$class_id,'class_name'=>$class_name);
 //   echo json_encode($response);
   
