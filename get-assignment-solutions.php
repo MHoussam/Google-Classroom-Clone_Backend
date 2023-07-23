7039352d25852a417ea3800cd66ce150eb632cf8
marcabounader92 <marcabounader92@outlook.com>
2023-07-23 16:14:57 +0300

    PHP: added num rows condition

--- get-assignment-solutions.php
+++ get-assignment-solutions.php
@@ -20,7 +20,7 @@ $result=$sql->get_result();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $arrayofrows = array();
 
-if(!$result){
+if($result->num_rows()==0){
   $response=array("status"=>"0","error"=>"No solutions");
   echo json_encode($data);
   exit();
