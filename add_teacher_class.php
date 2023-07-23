a5d92f1f2902c256c571ed9553a43294f3db89c7
marcabounader92 <marcabounader92@outlook.com>
2023-07-23 16:14:17 +0300

    PHP: added class new columns

--- add_teacher_class.php
+++ add_teacher_class.php
@@ -1,8 +1,15 @@
 <?php
+include('Config\db_connect.php');
+
+// $_POST = json_decode(file_get_contents('php://input'), true);
 $teacher_id=$_POST['teacher_id'];
 $class_name=$_POST['class_name'];
-$sql = $conn->prepare("insert into classes (teacher_id,class_name) values(?,?)");
-    $sql->bind_param("ss", $teacher_id,$class_name);
+$section=$_POST['section'];
+$subject=$_POST['subject'];
+$room=$_POST['room'];
+$meet_link=$_POST['meet_link'];
+$sql = $conn->prepare("insert into classes (teacher_id,class_name,section,subject,room,meet_link) values(?,?,?,?,?,?)");
+    $sql->bind_param("ssssss", $teacher_id,$class_name,$section,$subject,$room,$meet_link);
     $sql->execute();
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     if($sql->get_result()){
--- get-student-classes.php
+++ get-student-classes.php
@@ -12,14 +12,14 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 $student_id=$_GET['student_id'];
 $class_name=$teacher_id=$teacher_name="";
-$sql = $conn->prepare("select class_name from classes where class_id IN (select class_id from class_students where student_id=?);");
+$sql = $conn->prepare("select class_name,section,subject,room,meet_link from classes where class_id IN (select class_id from class_students where student_id=?);");
 $sql->bind_param("s",$student_id);
 $sql->execute();
 $result=$sql->get_result();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $arrayofrows = array();
 
-if(!$result){
+if($result->num_rows()=="0"){
   $response=array("status"=>"0","error"=>"No classes");
   echo json_encode($data);
   exit();
