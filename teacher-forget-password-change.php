1553cc1d1eb02860769c0f5d2b457de58761e0d4
marcabounader92 <marcabounader92@outlook.com>
2023-07-23 19:18:22 +0300

    PHP: added teacher forget reset

---
+++ teacher-forget-password-change.php
@@ -0,0 +1,41 @@
+<?php
+    include('Config\db_connect.php');
+
+    // $_POST = json_decode(file_get_contents('php://input'), true);
+    $email=$_POST['email'];
+    $reset_token=$_POST['reset_token'];
+    $new_password=$_POST['new_password'];
+    $sql = $conn->prepare("select reset_token,exp_date from password_reset_temps where email=?");
+    $sql->bind_param("s",$email);
+    $sql->execute();
+    $result=$sql->get_result();
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    if($result->num_rows=="0"){
+        $response=array("status"=>"0","error"=>"Email does not have token");
+        echo json_encode($response);
+        exit();
+    } else {
+        $stored_token='';
+        $exp_date='';
+        while( $row=$result->fetch_array(MYSQLI_ASSOC)){
+            $stored_token = $row['reset_token'];
+            $exp_date= $row['exp_date'];
+        }
+        if($stored_token==$reset_token){
+            $new_hashed_pass = password_hash($new_password, PASSWORD_BCRYPT);
+            $sql = $conn->prepare("UPDATE teachers SET password=? WHERE email=?");
+            $sql->bind_param("ss",$new_hashed_pass,$email);
+            $sql->execute();
+            if($sql->affected_rows=="0"){
+                $response=array('status'=>"0","result"=>"Could not change password");
+                echo json_encode($response);
+                exit();
+            }else{
+                $response=array('status'=>"1","result"=>"Password changed");
+                echo json_encode($response);
+                exit();
+            }
+        }
+    }
+
+?>
---
+++ teacher-forget-password.php
@@ -0,0 +1,48 @@
+<?php
+    include('Config\db_connect.php');
+    use PHPMailer\PHPMailer\PHPMailer;
+    require("PHPMailer\src\SMTP.php");
+    require("PHPMailer\src\PHPMailer.php");
+
+    // $_POST = json_decode(file_get_contents('php://input'), true);
+    $email=$_POST['email'];
+    $sql = $conn->prepare("select teacher_id from teachers where email=?");
+    $sql->bind_param("s",$email);
+    $sql->execute();
+    $result=$sql->store_result();
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    if($result->num_rows=="0"){
+        $response=array("status"=>"0","error"=>"Email does not exist");
+        echo json_encode($response);
+        exit();
+    }
+    $reset_token = bin2hex(openssl_random_pseudo_bytes(16));
+    $exp_date=date('h:i:s', time());
+    $sql = $conn->prepare("INSERT INTO password_reset_temps (email,reset_token,exp_date) VALUES (?,?,?)");
+    $sql->bind_param("sss",$email,$reset_token,$exp_date);
+    $sql->execute();
+    if($sql->affected_rows=="0"){
+        $response=array("status"=>"0","error"=>"Could not create token");
+        echo json_encode($response);
+        exit();
+    } 
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'sandbox.smtp.mailtrap.io';
+    $mail->SMTPAuth = true;
+    $mail->Port = 2525;
+    $mail->Username = '56ddaa55f02678';
+    $mail->Password = 'e5259f71c3dcb4';
+    $mail->setFrom('info@mailtrap.io', 'Mailtrap');
+    $mail->addAddress($email);
+    $mail->Subject = 'Password Recovery';
+    $mail->Body = $reset_token;
+    if(!$mail->send()){
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }else{
+        echo 'Message has been sent';
+    }
+
+?> 
