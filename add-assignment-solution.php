e8c10243e8d25037d2ff87eb13f80dfa4696a2b1
marcabounader92 <marcabounader92@outlook.com>
2023-07-24 13:16:26 +0300

    PHP: added student in class condition

--- add-assignment-solution.php
+++ add-assignment-solution.php
@@ -7,14 +7,24 @@ $student_id=$_POST['student_id'];
 $assignment_id=$_POST['assignment_id'];
 $solution=$_POST['solution'];
 
-$sql = $conn->prepare("select assignment_id from assignments where assignment_id=?");
-$sql->bind_param("s",$assignment_id);
+$sql = $conn->prepare("select student_id from students where student_id=?");
+$sql->bind_param("i", $student_id);
 $sql->execute();
 $sql->store_result();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+if($sql->num_rows=="0"){
+    $response=array('status'=>'0','error'=>'Student does not exist');
+    echo json_encode($response);
+    exit();
+}
 
+$sql = $conn->prepare("select class_id from class_students where student_id=? and class_id IN(select class_id from classes where class_id IN (select class_id from assignments where assignment_id=?))");
+$sql->bind_param("ii", $student_id,$assignment_id);
+$sql->execute();
+$sql->store_result();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if($sql->num_rows==0){
-  $data=array("status"=>"0","error"=>"Assignment does not exist");
+  $data=array("status"=>"0","error"=>"Student is not in this class or assignment does not exist");
   echo json_encode($data);
   exit();
 } else{
