67d62ce7dfe1f376ee90d959240c4190ea985e02
marcabounader92 <marcabounader92@outlook.com>
2023-07-24 21:20:22 +0300

    PHP: fixed semicolon bug

--- register-student.php
+++ register-student.php
@@ -6,7 +6,7 @@ include('Config\db_connect.php');
 header("Content-type: application/json; charset=utf-8");
 header('Access-Control-Allow-Origin: http://localhost:5500');
 header('Access-Control-Allow-Methods: POST');
-header("Access-Control-Allow-Headers: Content-Type")
+header("Access-Control-Allow-Headers: Content-Type");
 
 // $_POST = json_decode(file_get_contents('php://input'), true);
 
