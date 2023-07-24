e10f0ed62cfa2957b08bf07079838b406dbc7628
marcabounader92 <marcabounader92@outlook.com>
2023-07-24 21:14:52 +0300

    PHP: added get teacher class api

---
+++ get-class-teachers.php
@@ -0,0 +1,36 @@
+<?php
+
+include('Config\db_connect.php');
+
+header("Content-type: application/json; charset=utf-8");
+header('Access-Control-Allow-Origin: http://localhost:5500');
+header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Headers: Content-Type");
+
+if(isset($_GET['class_id'])){
+    $class_id=$_GET['class_id'];
+    $sql = $conn->prepare("select first_name,last_name from teachers where teacher_id IN (select teacher_id from class_teachers where class_id=?)");
+    $sql->bind_param("i",$class_id);
+    $sql->execute();
+    $result=$sql->get_result();
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $arrayofrows = array();
+    if($result->num_rows=="0"){
+        $response=array("status"=>"0","error"=>"No teachers");
+        echo json_encode($response);
+        exit();
+    } else {
+        while( $row=$result->fetch_array(MYSQLI_ASSOC)){
+            $arrayofrows[] = $row;
+        }
+        echo json_encode($arrayofrows);
+        exit();
+    }
+} else {
+    $response=array('status'=>0,'error'=>'Please provide class id');
+    echo json_encode($response);
+    exit();
+}
+
+
+?>
