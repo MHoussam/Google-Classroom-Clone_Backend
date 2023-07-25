5f3cbf9dcf5f56ee0b79cde10ea6014ea5e6cc81
marcabounader92 <marcabounader92@outlook.com>
2023-07-25 22:26:25 +0300

    PHP: google meet authentication added

--- get-class-google-meet.php
+++ get-class-google-meet.php
@@ -8,6 +8,11 @@ header('Access-Control-Allow-Methods: POST');
 header("Access-Control-Allow-Headers: Content-Type");
 
 $class_id=$_GET['class_id'];
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$token_value=$_POST['token_value'];
+
+include('student-authentication-validation.php');
 $meet_link="";
 $sql = $conn->prepare("select meet_link from classes where class_id=?");
 $sql->bind_param("i",$class_id);
