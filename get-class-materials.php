1076d7a9cc4f29bb008cf72e2630de951ba77023
marcabounader92 <marcabounader92@outlook.com>
2023-07-26 00:50:07 +0300

    PHP: order by desc

--- get-class-materials.php
+++ get-class-materials.php
@@ -16,7 +16,7 @@ $token_value=$_POST['token_value'];
 
 include('authentication-validation.php');
 $material_id=$title=$description=$path=$date_of_upload="";
-$sql = $conn->prepare("select DISTINCT material_id,title,date_of_upload,first_name,last_name from materials m join teachers t on m.teacher_id=t.teacher_id where class_id=?;");
+$sql = $conn->prepare("select DISTINCT material_id,title,date_of_upload,first_name,last_name from materials m join teachers t on m.teacher_id=t.teacher_id where class_id=? order by date_of_upload desc;");
 $sql->bind_param("i",$class_id);
 $sql->execute();
 $result=$sql->get_result();
