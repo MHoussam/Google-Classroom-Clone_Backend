5b2c59213d71b984448d1c6102ba78675ee156c2
marcabounader92 <marcabounader92@outlook.com>
2023-07-25 20:28:05 +0300

    PHP: student token validation

---
+++ student-authentication-validation.php
@@ -0,0 +1,21 @@
+<?php
+include('Config\db_connect.php');
+
+if(isset($_POST['token_value'])){
+    $token_value=$_POST['token_value'];
+    $sql = $conn->prepare("select token_id from student_tokens where token_value=?");
+    $sql->bind_param("s",$token_value);
+    $sql->execute();
+    $sql->store_result();
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    if($sql->num_rows=="0"){
+        $response=array("status"=>"0","error"=>"Authentication required");
+        echo json_encode($response);
+        exit();
+    }
+} else {
+    $response=array("status"=>"0","error"=>"Authentication required");
+    echo json_encode($response);
+    exit();   
+}
+?>
