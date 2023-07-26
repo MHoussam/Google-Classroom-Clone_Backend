ab2479f4312ed35c68746330ad74979bef8a5680
marcabounader92 <marcabounader92@outlook.com>
2023-07-26 17:35:10 +0300

    PHP: removed path

--- get-class-material-info.php
+++ get-class-material-info.php
@@ -15,8 +15,8 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 $token_value=$_POST['token_value'];
 
 include('authentication-validation.php');
-$title=$description=$path=$date_of_upload=$first_name=$last_name="";
-$sql = $conn->prepare("select title,description,path,date_of_upload,first_name,last_name from materials m join teachers t on m.teacher_id=t.teacher_id where material_id=?;");
+$title=$description=$date_of_upload=$first_name=$last_name="";
+$sql = $conn->prepare("select title,description,date_of_upload,first_name,last_name from materials m join teachers t on m.teacher_id=t.teacher_id where material_id=?;");
 $sql->bind_param("i",$material_id);
 $sql->execute();
 $sql->store_result();
@@ -27,9 +27,9 @@ if($sql->num_rows=="0"){
   echo json_encode($response);
   exit();
 } else{
-    $sql->bind_result($title,$description,$path,$date_of_upload,$first_name,$last_name);
+    $sql->bind_result($title,$description,$date_of_upload,$first_name,$last_name);
     $sql->fetch();
-    $response=array("status"=>"1","title"=>$title,'description'=>$description,"path"=>$path,"date_of_upload"=>$date_of_upload,"first_name"=>$first_name,"last_name"=>$last_name);
+    $response=array("status"=>"1","title"=>$title,'description'=>$description,"date_of_upload"=>$date_of_upload,"first_name"=>$first_name,"last_name"=>$last_name);
     echo json_encode($response);
     exit();
 }
