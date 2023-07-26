fc968bd287935efcc0291f989c7cf5c178281c5f
marcabounader92 <marcabounader92@outlook.com>
2023-07-26 06:52:57 +0300

    PHP: authentication bug fixed

--- authentication-validation.php
+++ authentication-validation.php
@@ -9,7 +9,7 @@ if(isset($_POST['token_value'])){
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     if($sql->num_rows=="0"){
         $token_value=$_POST['token_value'];
-        $sql = $conn->prepare("select token_id from student_tokens where token_value=?");
+        $sql = $conn->prepare("select token_id from teacher_tokens where token_value=?");
         $sql->bind_param("s",$token_value);
         $sql->execute();
         $sql->store_result();
