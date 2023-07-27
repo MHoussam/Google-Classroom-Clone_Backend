a04326ab70879b36649a3c216eda5b81efb1a5af
marcabounader92 <marcabounader92@outlook.com>
2023-07-27 18:37:04 +0300

    PHP: last commit

---
+++ get-user-info.php
@@ -0,0 +1,43 @@
+
+<?php
+
+include('Config\db_connect.php');
+
+header("Content-type: application/json; charset=utf-8");
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+header('Access-Control-Allow-Methods: GET,POST');
+header("Access-Control-Allow-Headers: Content-Type");
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$token_value=$_POST['token_value'];
+
+include('authentication-validation.php');
+
+//if(isset($_POST['class_id'])){
+    $student_id=$_POST['student_id'];
+    $sql = $conn->prepare("select first_name,last_name from students where student_id=?");
+    $sql->bind_param("i",$student_id);
+    $sql->execute();
+    $result=$sql->get_result();
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $arrayofrows = array();
+    if($result->num_rows=="0"){
+        $response=array("status"=>"0","error"=>"No students");
+        echo json_encode($response);
+        exit();
+    } else {
+        while( $row=$result->fetch_array(MYSQLI_ASSOC)){
+            $arrayofrows[] = $row;
+        }
+        echo json_encode($arrayofrows);
+        exit();
+    }
+// } else {
+//     $response=array('status'=>0,'error'=>'Please provide class id');
+//     echo json_encode($response);
+//     exit();
+// }
+
+
+?>
